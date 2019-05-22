609fc4ec8b3c12f359015c1d0709b9040f2509dd
Carlos H. Marian <carlos.marian@amcom.com.br>
2019-05-22 19:17:41 -0300

    First entity

---
+++ Models/Funcionario.cs
@@ -0,0 +1,11 @@
+namespace NetCOREEntityFramework.Models
+{
+    public class Funcionario
+    {
+        int id {get;set;}
+
+        string nome {get;set;}
+
+        float salario{get;set;}
+    }
+}
