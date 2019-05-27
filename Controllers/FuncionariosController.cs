3ba01d5fa40e57ca426b185a362326df617814f0
Carlos H. Marian <carlos.marian@amcom.com.br>
2019-05-27 18:01:57 -0300

    Configure persistence of entity Funcionario

--- Controllers/FuncionariosController.cs
+++ Controllers/FuncionariosController.cs
@@ -1,18 +1,29 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
 using NetCOREEntityFramework.Models;
+using NetCOREEntityFramework.Database;
 
 namespace NetCOREEntityFramework.Controllers
 {
     public class FuncionariosController : Controller
     {
+        private readonly ApplicationDBContext database;
+
+        public FuncionariosController(ApplicationDBContext database){
+            this.database = database;
+        }
+
         public IActionResult Cadastrar(){
             return View();
         }
 
         [HttpPost]
         public IActionResult Salvar(Funcionario funcionario){
-            return Content("OK"+ funcionario.ToString());
+            //Solicita a adição do item;
+            database.Funcionarios.Add(funcionario);
+            //Confirma a adição.
+            database.SaveChanges();
+            return Content("Funcionário salvo");
         }
     }
 }
